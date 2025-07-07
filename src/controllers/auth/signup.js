36c1dc364d1b4832e17c8dac88ecf92097c51b6f
Sahil <hanss9545@gmail.com>
2025-07-07 23:43:09 +0530

    setup postgresql with prisma ORM

--- src/controllers/auth/signup.js
+++ src/controllers/auth/signup.js
@@ -1,5 +1,8 @@
+import prisma from "../../config/prisma.js";
+
 // TODO : finish the signup controller
-export default (req, res) => {
-  console.log("request body : ", req.body);
+export default async (req, res) => {
+  const db = await prisma.$queryRaw`SELECT current_database()`;
+  console.log("CURRENT DB : ", db);
   res.json("Signup successfully done.");
 };
